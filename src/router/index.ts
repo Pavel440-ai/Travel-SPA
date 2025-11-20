fababbde72a13a6a54f89d4d7f87ddf6ba21ae11
Pavel <potapenkopavel343@gmail.com>
2025-11-20 12:49:10 +0100

    index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,41 +1,31 @@
-import { createRouter, createWebHistory, type RouterOptions } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DestinationView from '../views/DestinationView.vue'
 import ExperienceView from '../views/ExperienceView.vue'
 
-const routes: RouterOptions['routes'] = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView,
-  },
-  {
-    path: '/destination/:slug',
-    name: 'destination',
-    component: DestinationView,
-    props: true,
-    children: [
-      {
-        path: ':experienceSlug',
-        name: 'experience',
-        component: ExperienceView,
-        props: true,
-      },
-    ],
-  },
-]
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
-  scrollBehavior() {
-    return { top: 0 }
-  },
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/destination/:slug',
+      name: 'destination',
+      component: DestinationView,
+      props: true,
+      children: [
+        {
+          path: ':experienceSlug',
+          name: 'experience',
+          component: ExperienceView,
+          props: true,
+        },
+      ],
+    },
+  ],
 })
 
 export default router
-
-
-
-
-
